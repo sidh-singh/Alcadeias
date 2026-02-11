217eddcf4e7166240eeaf19405073f3480cd62e4
Sidharth Singh <thenewsidharth@gmail.com>
2026-02-12 00:03:12 +0530

    fix: Correct trend power list references in buy/sell status logic

--- strategy.py
+++ strategy.py
@@ -196,7 +196,7 @@ class Strategy:
         elif buy_count > 0 and sell_count == 0:
             if buy_profit > self.hedge:
                 buy_status = Signal.CLOSE_BUY
-            elif lt_trend_power_list[0] == 0:
+            elif lt_sha_power_list[0] == 0:
                 buy_status = Signal.CLOSE_BUY
             elif buy_first_profit < -(self._get_fibo_qty(buy_count, times) ** 3):
                 buy_status = Signal.BUY_MORE
@@ -205,7 +205,7 @@ class Strategy:
         elif buy_count == 0 and sell_count > 0:
             if sell_profit > self.hedge:
                 sell_status = Signal.CLOSE_SELL
-            elif lt_trend_power_list[0] == 1:
+            elif lt_sha_power_list[0] == 1:
                 sell_status = Signal.CLOSE_SELL
             elif sell_first_profit < -(self._get_fibo_qty(sell_count, times) ** 3):
                 sell_status = Signal.SELL_MORE
