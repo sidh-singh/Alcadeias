d3d92d099fdd7e3de0df41bcc3491db2d6fd55f0
Sidharth Singh <thenewsidharth@gmail.com>
2026-02-19 05:48:36 -0800

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -246,7 +246,7 @@ class Strategy:
         elif buy_count > 0 and sell_count == 0:
             if buy_profit > 2:
                 buy_status = Signal.CLOSE_BUY
-            elif (lt_trend_power_list[0] == 0) and (buy_first_profit <= -35):
+            elif (lt_trend_power_list[0] == 0) and (buy_first_profit <= -45):
                 buy_status = Signal.CLOSE_BUY
             elif buy_first_profit < -(self._get_fibo_qty(buy_count, 1) ** fibo_power):
                 buy_status = Signal.BUY_MORE
@@ -255,7 +255,7 @@ class Strategy:
         elif buy_count == 0 and sell_count > 0:
             if sell_profit > 2:
                 sell_status = Signal.CLOSE_SELL
-            elif (lt_trend_power_list[0] == 1) and (sell_first_profit <= -35):
+            elif (lt_trend_power_list[0] == 1) and (sell_first_profit <= -45):
                 sell_status = Signal.CLOSE_SELL
             elif sell_first_profit < -(self._get_fibo_qty(sell_count, 1) ** fibo_power):
                 sell_status = Signal.SELL_MORE
