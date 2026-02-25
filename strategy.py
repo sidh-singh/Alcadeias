b61a3bf7b89426c78da1c53a8b408a3210639b7d
Sidharth <thenewsidharth@gmail.com>
2026-02-26 00:40:32 +0530

    Refine closing conditions for buy and sell statuses by adjusting profit thresholds

--- strategy.py
+++ strategy.py
@@ -256,7 +256,7 @@ class Strategy:
         elif buy_count > 0 and sell_count == 0:
             if buy_profit > close_threshold:
                 buy_status = Signal.CLOSE_BUY
-            elif (lt_sha_power_list[0] == 0) or (buy_first_profit < -25) or exit_conv_ok:
+            elif (buy_first_profit < -50) or exit_conv_ok:
                 buy_status = Signal.CLOSE_BUY
             elif buy_first_profit < -(self._get_fibo_qty(buy_count, 1) ** fibo_power):
                 buy_status = Signal.BUY_MORE
@@ -265,7 +265,7 @@ class Strategy:
         elif buy_count == 0 and sell_count > 0:
             if sell_profit > close_threshold:
                 sell_status = Signal.CLOSE_SELL
-            elif (lt_sha_power_list[0] == 1) or (sell_first_profit < -25) or exit_conv_ok:
+            elif (sell_first_profit < -50) or exit_conv_ok:
                 sell_status = Signal.CLOSE_SELL
             elif sell_first_profit < -(self._get_fibo_qty(sell_count, 1) ** fibo_power):
                 sell_status = Signal.SELL_MORE
